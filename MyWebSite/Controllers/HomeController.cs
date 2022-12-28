5e6e891fc8eb256db13c2519c7486346757d0b01
rasmane226 <yameogorasmane99@gmail.com>
2022-12-28 21:17:07 +0300

    Proje dosyası ekle.

---
+++ MyWebSite/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyWebSite.Models;
+using System.Diagnostics;
+
+namespace MyWebSite.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult home()
+        {
+            return View();
+        }
+    }
+}
